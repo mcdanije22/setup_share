4f5a09ed0f0796a38ca45e74b46f0a438f250e64
mcdanije22 <mcdanije22@gmail.com>
2021-08-21 01:07:24 -0400

    routes for users working correctly along with adding tokens and cookies

--- server/migrations/20210801053746_user_table.js
+++ server/migrations/20210801053746_user_table.js
@@ -3,11 +3,11 @@ exports.up = function (knex) {
 
   return knex.schema.createTable("users", (table) => {
     table.uuid("userID").defaultTo(knex.raw("uuid_generate_v4()")).primary();
-    table.string("username");
-    table.string("email");
-    table.string("password");
-    table.string("first_name");
-    table.string("last_name");
+    table.string("username").notNullable().unique();
+    table.string("email").unique().notNullable();
+    table.string("password").notNullable();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
     table.dateTime("user_created_date").defaultTo(date);
     table.dateTime("subscription_exp_date").defaultTo(date);
   });
--- server/routes/userRouters.ts
+++ server/routes/userRouters.ts
@@ -2,30 +2,89 @@ import express, { Router } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import db from "../services/dbConnection";
+import cookieParser from "cookie-parser";
+
+express().use(cookieParser());
 
 const userRouter = Router();
 
 userRouter.post("/register", (req, res) => {
   const saltRounds = 10;
   const { username, email, createdPassword, first_name, last_name } = req.body;
-  const myPlaintextPassword = createdPassword;
-  bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
-    // const newUser = {
-    //   username,
-    //   email,
-    //   hashedPassword: hash,
-    //   first_name,
-    //   last_name,
-    // };
-    db.insert({
-      username,
-      email,
-      password: hash,
-      first_name,
-      last_name,
-    }).into("users");
-    res.send({ message: "user created" });
+  bcrypt.hash(createdPassword, saltRounds, function (err, hash) {
+    (async function () {
+      try {
+        const createUser = db("users")
+          .insert({
+            username,
+            email,
+            password: hash,
+            first_name,
+            last_name,
+          })
+          .returning("*");
+        const data = await createUser;
+        res.send({ data });
+      } catch (error) {
+        console.log(error);
+        if (error.constraint === "users_email_unique") {
+          res.send({ message: "email already has an account" });
+        } else if (error.constraint === "users_username_unique") {
+          res.send({ message: "username already taken" });
+        } else {
+          res.send({ message: error.detail });
+        }
+      }
+    })();
   });
+});
+userRouter.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const getUserHash = () => {
+    const userHash = db("users").select("password").where("email", email);
+    return userHash;
+  };
+  const logUserIn = async () => {
+    const user = await db("users")
+      .select(
+        "userID",
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "user_created_date",
+        "subscription_exp_date"
+      )
+      .where("email", email);
+    createToken(user[0].userID);
+    res.send({ user });
+  };
+  const createToken = (uniqueData) => {
+    const token = jwt.sign(
+      {
+        data: uniqueData,
+      },
+      "secret", //change this to much longer string and make .env var
+      { expiresIn: "1h" }
+    );
+    res.cookie("token", token, {
+      maxAge: 900000,
+      httpOnly: true,
+    });
+  };
+
+  (async function () {
+    const dbHashPassword = await getUserHash();
+    const hashedPassword = await dbHashPassword[0].password;
+    bcrypt.compare(password, hashedPassword, function (err, result) {
+      if (result) {
+        console.log("success");
+        logUserIn();
+      } else {
+        res.send({ message: "Incorrect password!" });
+      }
+    });
+  })();
 });
 
 export default userRouter;
