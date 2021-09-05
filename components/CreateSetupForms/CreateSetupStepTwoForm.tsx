7e14f0735966fe2c6dcf9bb9621cbc657305f1e8
mcdanije22 <mcdanije22@gmail.com>
2021-09-05 16:42:54 -0400

    changes to file upload style

--- components/CreateSetupForms/CreateSetupStepTwoForm.tsx
+++ components/CreateSetupForms/CreateSetupStepTwoForm.tsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
-import { Upload, Row, Col, Typography, Space, Button } from "antd";
+import { Upload, Row, Col, Typography, Space, Button, message } from "antd";
 import ImgCrop from "antd-img-crop";
 import styles from "./createRoomForms.module.scss";
-import { ArrowRightOutlined, ArrowLeftOutlined } from "@ant-design/icons";
+import {
+  ArrowRightOutlined,
+  ArrowLeftOutlined,
+  InboxOutlined,
+  CheckCircleTwoTone,
+} from "@ant-design/icons";
 
 const { Title } = Typography;
+const { Dragger } = Upload;
 
 const CreateSetupStepTwoForm = () => {
   const [fileList, setFileList] = useState([]);
@@ -30,27 +36,46 @@ const CreateSetupStepTwoForm = () => {
   console.log(fileList);
   return (
     <div id={styles.stepTwoFormContainer}>
-      <Space size={30} direction="vertical" style={{ width: "100%" }}>
+      <Space
+        size={30}
+        direction="vertical"
+        style={{ width: "100%", minHeight: "100vh" }}
+      >
         <Col sm={24}>
           <Title level={2} style={{ textAlign: "center", paddingTop: "2rem" }}>
             Upload Three Photos
           </Title>
         </Col>
-        <Row
-          justify="center"
-          style={{ minHeight: "80vh", paddingBottom: "2rem" }}
-        >
-          <Col>
+        <Row justify="center" style={{ paddingBottom: "2rem" }}>
+          <Col style={{ width: "100%" }}>
             <ImgCrop rotate>
-              <Upload
+              <Dragger
                 // action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-                listType="picture-card"
+                listType="picture"
                 fileList={fileList}
                 onChange={onChange}
                 onPreview={onPreview}
               >
-                {fileList.length < 3 && "+ Upload"}
-              </Upload>
+                {fileList.length < 3 && (
+                  <>
+                    <p className="ant-upload-drag-icon">
+                      <InboxOutlined />
+                    </p>
+                    <p className="ant-upload-text">Drag and Drop or Browse</p>
+                  </>
+                )}
+                {fileList.length >= 3 && (
+                  <>
+                    <p className="ant-upload-drag-icon">
+                      <CheckCircleTwoTone twoToneColor="#52c41a" />
+                    </p>
+                    <p className="ant-upload-text">Max photos selected</p>
+                    <p className="ant-upload-text">
+                      Delete Photo(s) or Continue
+                    </p>
+                  </>
+                )}
+              </Dragger>
             </ImgCrop>
           </Col>
         </Row>
