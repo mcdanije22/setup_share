598bc44dff1b8148e0454c7efffa8653f26f8752
mcdanije22 <mcdanije22@gmail.com>
2021-09-14 19:31:13 -0400

    logic for adding multiple items to image

--- components/CanvasImg.tsx
+++ components/CanvasImg.tsx
@@ -1,106 +1,20 @@
-// import React, { useState, useEffect, useRef } from "react";
-// import styles from "./Canvasimg.module.scss";
-// import ImageMapper from "react-image-mapper";
-
-// const CanvasImg = ({ imgSource }) => {
-//   const [x, setXCoord] = useState(0);
-//   const [y, setYCoord] = useState(0);
-//   const [editStatus, setEditStatus] = useState(false);
-//   const [testList, setList] = useState([218, 77, 214, 120, 264, 119]);
-//   const [tempCoordList, setTempCoordList] = useState([]);
-
-//   const MAP = {
-//     name: "my-map",
-//     areas: [
-//       {
-//         name: "1",
-//         shape: "poly",
-//         coords: [25, 33, 27, 300, 128, 240, 128, 94],
-//         preFillColor: "green",
-//         fillColor: "blue",
-//       },
-//       {
-//         name: "2",
-//         shape: "poly",
-//         coords: [219, 118, 220, 210, 283, 210, 284, 119],
-//         preFillColor: "pink",
-//       },
-//       {
-//         name: "3",
-//         shape: "poly",
-//         coords: [381, 241, 383, 94, 462, 53, 457, 282],
-//         fillColor: "yellow",
-//       },
-//       {
-//         name: "4",
-//         shape: "poly",
-//         coords: [245, 285, 290, 285, 274, 239, 249, 238],
-//         preFillColor: "red",
-//       },
-//       { name: "5", shape: "circle", coords: [170, 100, 25] },
-//     ],
-//   };
-//   const MAP2 = {
-//     name: "my-map",
-//     areas: [
-//       {
-//         name: "1",
-//         shape: "poly",
-//         coords: [...testList],
-//         preFillColor: "green",
-//         fillColor: "blue",
-//       },
-//     ],
-//   };
-
-//   const onMove = (e) => {
-//     setXCoord(e.nativeEvent.offsetX);
-//     setYCoord(e.nativeEvent.offsetY);
-//   };
-
-//   const addTo = (coords) => {
-//     setList([...testList, ...coords]);
-//     console.log(testList);
-//   };
-//   return (
-//     <div id="imgContainer">
-//       <ImageMapper
-//         src={imgSource}
-//         map={MAP2}
-//         width={500}
-//         onMouseEnter={(area) => {
-//           alert("test");
-//         }}
-//         onImageClick={(e) => {
-//           addTo([e.nativeEvent.offsetX, e.nativeEvent.offsetY]);
-//           console.log(MAP2.areas[0]);
-//           //fixes lagging state for preview by updating local MAP2 object and state in same function
-//           MAP2.areas[0].coords.push(
-//             e.nativeEvent.offsetX,
-//             e.nativeEvent.offsetY
-//           );
-//         }}
-//       />
-//       <p>{x}</p>
-//       <p>{y}</p>
-//     </div>
-//   );
-// };
-// export default CanvasImg;
 import React, { useState, useEffect, useRef } from "react";
 import { Button, Form, Input, Row, Col } from "antd";
 import styles from "./Canvasimg.module.scss";
 import ImageMapper from "react-image-mapper";
 
 const CanvasImg = ({ imgSource }: any) => {
-  const [testList, setList] = useState([218, 77, 214, 120, 264, 119]);
   const [tempCoordList, setTempCoordList] = useState([]);
   const [drawingStatus, setDrawingStatus] = useState(false);
   const [addItemStatus, setAddItemStatus] = useState(false);
+  const [tempAreas, setTempAreas] = useState([]);
 
   const MAP2 = {
     name: "image-map",
-    areas: [
+    areas: [...tempAreas],
+  };
+  const startItemAdd = () => {
+    setTempAreas([
       {
         name: "1",
         shape: "poly",
@@ -108,13 +22,8 @@ const CanvasImg = ({ imgSource }: any) => {
         preFillColor: "green",
         fillColor: "blue",
       },
-    ],
-  };
-  const addTo = (coords) => {
-    setList([...testList, ...coords]);
-    console.log(testList);
-  };
-  const addItem = () => {
+      ...tempAreas,
+    ]);
     setAddItemStatus(true);
   };
   const startDrawing = () => {
@@ -127,12 +36,15 @@ const CanvasImg = ({ imgSource }: any) => {
   };
   const undoLastCoord = () => {
     MAP2.areas[0].coords.pop();
-    console.log(MAP2.areas[0].coords);
-
     MAP2.areas[0].coords.pop();
-    console.log(MAP2.areas[0].coords);
     setTempCoordList([...MAP2.areas[0].coords]);
   };
+  const finishAddItem = () => {
+    setAddItemStatus(false);
+    setDrawingStatus(false);
+    setTempCoordList([]);
+  };
+  console.log(MAP2);
   return (
     <div id="imgContainer">
       <Row justify="center">
@@ -186,7 +98,7 @@ const CanvasImg = ({ imgSource }: any) => {
                 </Form.Item>
               </>
             ) : (
-              <Button type="primary" onClick={addItem}>
+              <Button type="primary" onClick={startItemAdd}>
                 Add Item
               </Button>
             )}
@@ -203,7 +115,7 @@ const CanvasImg = ({ imgSource }: any) => {
                 </Col>
                 <Col>
                   <Form.Item>
-                    <Button htmlType="submit" type="primary">
+                    <Button onClick={finishAddItem} type="primary">
                       Submit Item
                     </Button>
                   </Form.Item>
