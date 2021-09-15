7bfbab433c032291973c92fe349f10a3e3d01682
mcdanije22 <mcdanije22@gmail.com>
2021-09-14 21:49:43 -0400

    logic for adding names/urls to items. uuid added as well

--- components/CanvasImg.tsx
+++ components/CanvasImg.tsx
@@ -2,12 +2,15 @@ import React, { useState, useEffect, useRef } from "react";
 import { Button, Form, Input, Row, Col } from "antd";
 import styles from "./Canvasimg.module.scss";
 import ImageMapper from "react-image-mapper";
+import { v4 as uuidv4 } from "uuid";
 
 const CanvasImg = ({ imgSource }: any) => {
   const [tempCoordList, setTempCoordList] = useState([]);
   const [drawingStatus, setDrawingStatus] = useState(false);
   const [addItemStatus, setAddItemStatus] = useState(false);
   const [tempAreas, setTempAreas] = useState([]);
+
+  const [form] = Form.useForm();
 
   const MAP2 = {
     name: "image-map",
@@ -16,7 +19,8 @@ const CanvasImg = ({ imgSource }: any) => {
   const startItemAdd = () => {
     setTempAreas([
       {
-        name: "1",
+        id: uuidv4(),
+        name: "",
         shape: "poly",
         coords: [...tempCoordList],
         preFillColor: "green",
@@ -39,10 +43,18 @@ const CanvasImg = ({ imgSource }: any) => {
     MAP2.areas[0].coords.pop();
     setTempCoordList([...MAP2.areas[0].coords]);
   };
-  const finishAddItem = () => {
+  const onReset = () => {
+    form.resetFields();
+  };
+  const finishAddItem = (values) => {
+    tempAreas[0].name = values.name;
+    tempAreas[0].url = values.url;
+    // setTempAreas((tempAreas[0] = updatedArea));
+    console.log(tempAreas);
     setAddItemStatus(false);
     setDrawingStatus(false);
     setTempCoordList([]);
+    onReset();
   };
   console.log(MAP2);
   return (
@@ -73,9 +85,10 @@ const CanvasImg = ({ imgSource }: any) => {
         <Col span={20}>
           <Form
             name="basic"
+            form={form}
             labelCol={{ span: 8 }}
             wrapperCol={{ span: 16 }}
-            // onFinish={onFinish}
+            onFinish={finishAddItem}
           >
             {addItemStatus ? (
               //form would be in fragments instead
@@ -115,7 +128,7 @@ const CanvasImg = ({ imgSource }: any) => {
                 </Col>
                 <Col>
                   <Form.Item>
-                    <Button onClick={finishAddItem} type="primary">
+                    <Button htmlType="submit" type="primary">
                       Submit Item
                     </Button>
                   </Form.Item>
