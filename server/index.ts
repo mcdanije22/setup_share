3e0bfb933b044d8ca512740dc1ac11092c21b863
Josh McDaniel <mcdanije22@gmail.com>
2022-04-30 14:47:07 -0400

    old file removal

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,3 @@
-import testHandler from "./controllers/testHandler";
-import uploadHandler from "./controllers/uploadHandler";
-import multerUploadHandler from "./controllers/multerUploadHandler";
-import uploadFile from "./services/s3_upload";
-import createUserHandler from "./controllers/createUserHandler";
-import db from "./services/dbConnection";
 import imageRoutes from "./routes/imageRoutes";
 import userRoutes from "./routes/userRouters";
 import setupRoutes from "./routes/setupRoutes";
@@ -24,22 +18,5 @@ app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use("/image", imageRoutes);
 app.use("/user", userRoutes);
 app.use("/setup", setupRoutes);
-
-// app.get("/ping", (req, res) => {
-//   const testFunction = async () => {
-//     const test = await db.select("*").from("users").where("username", "bjosh");
-//     console.log(test);
-//     const test2 = await db("users")
-//       .where("username", "bjosh")
-//       .select("username", "email");
-//     res.send({ test2 });
-//   };
-//   testFunction();
-//   console.log("new test");
-// });
-// app.get("/ping/:id", testHandler);
-// app.get("/upload", uploadHandler);
-// app.post("/images", upload.single("image-file"), multerUploadHandler);
-// app.post("/createuser", createUserHandler);
 
 app.listen(PORT, () => console.log(`server started successfully on ${PORT}`));
