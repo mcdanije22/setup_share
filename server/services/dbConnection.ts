1708053d20392168a489246bd590fd27a0147e74
Josh McDaniel <mcdanije22@gmail.com>
2022-05-22 19:40:57 -0400

    db settings

--- server/services/dbConnection.ts
+++ server/services/dbConnection.ts
@@ -2,7 +2,7 @@ const db = require("knex")({
   client: "pg",
   version: "7.2",
   connection: {
-    host: "host.docker.internal",
+    host: "172.17.0.1:5432",
     user: "root",
     password: "password",
     database: "share_station",
