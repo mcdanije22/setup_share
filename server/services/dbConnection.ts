8410bc9ffe4c3dda8cfa9c1fea4840fcf0f2756e
Josh McDaniel <mcdanije22@gmail.com>
2022-05-22 19:45:16 -0400

    db

--- server/services/dbConnection.ts
+++ server/services/dbConnection.ts
@@ -2,7 +2,7 @@ const db = require("knex")({
   client: "pg",
   version: "7.2",
   connection: {
-    host: "172.17.0.1:5432",
+    host: "172.17.0.1",
     user: "root",
     password: "password",
     database: "share_station",
