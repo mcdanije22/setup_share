34c48a506a645d97d7d26fe5022351d0f3cec7e2
Josh McDaniel <mcdanije22@gmail.com>
2022-05-24 20:56:18 -0400

    docker

--- server/services/dbConnection.ts
+++ server/services/dbConnection.ts
@@ -2,7 +2,7 @@ const db = require("knex")({
   client: "pg",
   version: "7.2",
   connection: {
-    host: "postgresql_database",
+    host: "172.17.0.1",
     user: "root",
     password: "password",
     database: "share_station",
