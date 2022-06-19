1412dc67d83bd16dee3b81b9b0d68096601a27dd
Josh McDaniel <mcdanije22@gmail.com>
2022-06-19 12:50:00 -0400

    test

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,7 @@ import setupRoutes from "./routes/setupRoutes";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+const db = require("./db");
 
 const app = express();
 
@@ -12,6 +13,11 @@ const PORT = process.env.PORT || 5000;
 
 // app.use(bodyParser.json());
 app.use(express.json());
+
+app.get("/setups", async (req, res) => {
+  const setups = await db.select().from("setups");
+  res.json(setups);
+});
 
 // need to make sure to specify origin or cors will block cookies
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
